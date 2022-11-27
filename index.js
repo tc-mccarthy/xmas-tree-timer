1776ec33eb855b2747f17da26ae9e897a95bf64f
TC McCarthy <tc.mccarthy1@gmail.com>
2022-11-26 23:33:29 -0500

    Additional logging

--- index.js
+++ index.js
@@ -14,16 +14,19 @@ async function run() {
     const plug = await client.getDevice({ host: "172.16.3.18" });
     const status = await plug.getSysInfo();
     const timeout = 1;
+    
+    console.log(new Date());
 
     // if the plug is on, turn it off for 5 seconds and then turn it back on
     if (status.relay_state) {
-      console.log(new Date());
       console.log("Outlet is on, cycling power");
       await plug.setPowerState(false);
       console.log(`Waiting ${timeout} seconds`);
       await timer(timeout);
       console.log("Turning it back on");
       await plug.setPowerState(true);
+    } else {
+      console.log("Outlet is off. Doing nothing");
     }
   } catch (e) {
     console.log(">> ERROR >>", e);
